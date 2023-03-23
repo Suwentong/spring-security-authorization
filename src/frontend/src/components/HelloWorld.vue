a438defbe35baea0f855dcfd2868c44f6465993d
Suwentong <abdukadirov048@gmail.com>
2023-03-23 14:03:41 +0800

    Optimized imports

--- src/frontend/src/components/HelloWorld.vue
+++ src/frontend/src/components/HelloWorld.vue
@@ -1,21 +1,45 @@
+
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <div>
+        <label for="email">Email:</label>
+        <input type="text" id="email" v-model="email">
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit" @click="login">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'HelloWorld',
   data() {
     return {
-      msg: ''
-    }
+      email: "",
+      password: "",
+    };
   },
-  mounted() {
-    fetch("/api/messages/hello")
-      .then((response) => response.text())
-      .then((data) => {
-        this.msg = data;
-      })
-  }
-}
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post("/api/v1/auth/authenticate", {
+          email: this.email,
+          password: this.password,
+        });
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        this.$router.push("api/v1/demo-controller");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
